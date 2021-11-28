ac124bfa050efe127b73f55cc1cc6a3bf05a147b
rloterh <rloterh@gmail.com>
2021-11-28 17:13:02 +0000

    Add currency dominance details to Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../assets/logo.png';
 import './Hero.scss';
 
-const Hero = () => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = (props) => {
+  const { btcDominance, ethDominance } = props;
+  return (
+    <div className="hero-container d-flex" data-testid="global-container">
+      <div className="d-data-1">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="d-data-2">
+        <h2>::Currency Dominance::</h2>
+        <p>
+          {'BTC: '}
+          {btcDominance}
+          %
+        </p>
+        <p>
+          {'ETH: '}
+          {ethDominance}
+          %
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  btcDominance: PropTypes.string.isRequired,
+  ethDominance: PropTypes.string.isRequired,
+};
 
 export default Hero;

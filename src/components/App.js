3d656de4ef9e111c9d845657b731fd0123044396
rloterh <rloterh@gmail.com>
2021-11-28 14:41:32 +0000

    Implement basic routes on components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
+import CurrencyDetails from './CurrencyDetails/CurrencyDetails';
+import './App.scss';
 
 const App = () => (
   <div className="App">
-    <p>
-      Crypto Index
-
-    </p>
-    <Home />
+    <Router>
+      <p>Crypto Index Nav</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/CurrencyDetial" element={<CurrencyDetails />} />
+      </Routes>
+    </Router>
   </div>
 );
 
